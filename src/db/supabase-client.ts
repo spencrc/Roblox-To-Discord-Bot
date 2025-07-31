b44a6edcce90a015353a3d368bab8906af531059
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-31 09:30:27 -0700

    added supabase postgres changes listener

--- src/db/supabase-client.ts
+++ src/db/supabase-client.ts
@@ -3,17 +3,27 @@ import { SUPABASE_URL, SUPABASE_KEY } from '../config.js';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
+interface PostgresChanges {
+	schema: string;
+	table: string;
+	commit_timestamp: string; //formatted as a string by supabase-js
+	eventType: 'INSERT' | 'DELETE' | 'UPDATE';
+	new: { [key: string]: string };
+	old: { [key: string]: string };
+	errors: unknown;
+}
+
 const channel = supabase
-  .channel('roblox_discord_links_changes')
-  .on(
-    'postgres_changes',
-    {
-      event: '*', // can be 'INSERT', 'UPDATE', 'DELETE'
-      schema: 'public',
-      table: 'roblox_discord_links',
-    },
-    (payload) => {
-      console.log('Change received!', payload);
-    }
-  )
-  .subscribe();
+	.channel('roblox_discord_links_changes')
+	.on(
+		'postgres_changes',
+		{
+			event: '*', // can be 'INSERT', 'UPDATE', 'DELETE'
+			schema: 'public',
+			table: 'roblox_discord_links'
+		},
+		(payload: PostgresChanges) => {
+			console.log('Change received!', payload);
+		}
+	)
+	.subscribe();
